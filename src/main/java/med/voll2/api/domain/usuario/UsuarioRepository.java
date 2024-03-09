ce042a961b44094b76d29f1c9b86cd9c2149dede
Allan <allancalixto1@gmail.com>
2024-03-09 14:58:26 -0300

    feature: login usuario

---
+++ src/main/java/med/voll2/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package med.voll2.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
