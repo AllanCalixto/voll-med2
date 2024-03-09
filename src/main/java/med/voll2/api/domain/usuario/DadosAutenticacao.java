43d43a53a5a2ace88f6210a6080155f15079c8e0
Allan <allancalixto1@gmail.com>
2024-03-09 14:57:47 -0300

    feature: Crud

---
+++ src/main/java/med/voll2/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package med.voll2.api.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
---
+++ src/main/java/med/voll2/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package med.voll2.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
