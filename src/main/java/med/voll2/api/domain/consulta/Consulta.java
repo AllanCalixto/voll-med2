fb89f84cb1445a1f7ed4bf8dba82c99d776e23d1
Allan <allancalixto1@gmail.com>
2024-03-10 20:37:33 -0300

    feature: consulta

---
+++ src/main/java/med/voll2/api/domain/consulta/Consulta.java
@@ -0,0 +1,32 @@
+package med.voll2.api.domain.consulta;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll2.api.domain.medico.Medico;
+import med.voll2.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Consulta")
+@Table(name = "consultas")
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+}
